3431604594c670a6bbf6b65850c017a602640e02
Alishaaa <144117849+Alina621@users.noreply.github.com>
2024-01-24 16:45:43 +0300

    Add files via upload

---
+++ Untitled-2.js
@@ -0,0 +1,7 @@
+let myDiv = document.getElementById('myDiv');
+let myButton = document.getElementById('myButton');
+
+myButton.addEventListener('click', function() {
+  myDiv.style.width = '400px';
+  myDiv.style.height = '300px';
+});
